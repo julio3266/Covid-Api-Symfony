cc4e3cabc38ba71922965464e8e266e8ed40f13b
Julio Valente Costa <52897217+julio3266@users.noreply.github.com>
2020-05-19 09:50:29 -0300

    indexAction puxando states para o index

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Entity\City;
+use App\Entity\State;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ObjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,14 +15,24 @@ class IndexController extends AbstractController
 {
     private EntityManager $entityManager;
     private ObjectRepository $cityRepository;
+    private ObjectRepository $stateRepository;
 
     public function __construct(EntityManager $entityManager)
     {
         $this->entityManager = $entityManager;
         $this->cityRepository = $entityManager->getRepository(City::class);
+
+        $this->stateRepository = $entityManager->getRepository(State::class);
     }
     public function indexAction(): Response
     {
+        $states = $this->stateRepository->findAll();
+        $dados = [];
+
+        foreach ($states as $state) {
+            $dadps[$state->getName()] = $dados[$state->getName()] ?? [];
+        }
+
         return $this->render("index/index.html.twig");
     }
     public function cityAction(): Response
