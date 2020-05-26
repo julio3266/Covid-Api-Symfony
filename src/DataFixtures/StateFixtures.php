60f55213989d11cba6f413e3df0ad375b51486f5
Julio Valente Costa <52897217+julio3266@users.noreply.github.com>
2020-05-25 21:35:59 -0300

    States in regions on fixtures

--- src/DataFixtures/StateFixtures.php
+++ src/DataFixtures/StateFixtures.php
@@ -22,6 +22,15 @@ class StateFixtures extends Fixture implements DependentFixtureInterface
         /** @var Region $norte */
         $norte = $regionRepository->findOneBy(["name"=>"Norte"]);
 
+        /** @var Region $centroOeste */
+        $centroOeste = $regionRepository->findOneBy(["name"=>"Centro-Oeste"]);
+
+        /** @var Region $sudeste */
+        $sudeste = $regionRepository->findOneBy(["name"=>"Sudeste"]);
+
+        /** @var Region $sul */
+        $sul = $regionRepository->findOneBy(["name"=>"Sul"]);
+
         $states = [
             new State("Ceará", "ce", $nordeste),
             new State("Maranhão", "ma", $nordeste),
@@ -34,6 +43,26 @@ class StateFixtures extends Fixture implements DependentFixtureInterface
             new State("Sergipe","se", $nordeste),
 
             new State("Amazonas", "am", $norte),
+            new State("Amapá", "ap", $norte),
+            new State("Acre", "ac", $norte),
+            new State("Pará", "pa", $norte),
+            new State("Rondônia", "ro", $norte),
+            new State("Roraima", "rr", $norte),
+            new State("Tocantins", "to", $norte),
+
+            new State("Distrito Federa", "df", $centroOeste),
+            new State("Goiás", "go", $centroOeste),
+            new State("Mato Grosso", "mt", $centroOeste),
+            new State("Mato Grosso do Sul", "ms", $centroOeste),
+
+            new State("Espírito Santo", "es", $sudeste),
+            new State("Minas Gerais", "mg", $sudeste),
+            new State("Rio de Janeiro", "rj", $sudeste),
+            new State("São Paulo", "sp", $sudeste),
+
+            new State("Paraná", "pr", $sul),
+            new State("Rio Grande do Sul", "rs", $sul),
+            new State("Santa Catarina", "sc", $sul),
         ];
         foreach ($states as $state)
         {
